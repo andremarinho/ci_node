ed6cb325222d4930f39055fc7e58877d0b88dd1a
andre.marinho <andre.marinho@reclameaqui.com.br>
2025-01-08 12:48:40 -0300

    feat: inicial commit

---
+++ src/soma.js
@@ -0,0 +1,7 @@
+console.log(soma(10,10));
+
+function soma(a, b) {
+    return a + b;
+  }
+  
+  module.exports = soma;
---
+++ src/soma.test.js
@@ -0,0 +1,13 @@
+const soma = require('./soma');
+
+test("add 1 + 2 to be equal 3", ()=> {
+    expect(soma(1,2)).toBe(3);
+});
+
+test("add 2 + 2 to be equal 4", ()=> {
+    expect(soma(2,2)).toBe(4);
+});
+
+test("add 2 + 3 to be equal 5", ()=> {
+    expect(soma(2,3)).toBe(5);
+});
